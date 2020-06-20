ae1fabdb6dc4fe9d33ab48665fa900c4b5506f4e
Attamjot <attamjotsingh91@gmail.com>
2020-06-20 12:46:18 +0530

    updated app file and package json

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,24 @@ import React from 'react';
 import image from '../../public/assets/history.svg';
 
 class App extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        name: "React Application"
-      }
-    }
-    render() {
-        const { name } = this.state;
-        return (
-          <div class="App">
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'React Application',
+    };
+  }
+  render() {
+    const { name } = this.state;
+    return (
+      <div class="App">
+        <h1 className="main-header">My {name}</h1>
 
-            <h1 className="main-header">My {name}</h1>
-            
-            <div>
-              <img src={image} alt="react logo" />
-            </div>
-          </div>
-        );
-    }
+        <div>
+          <img src={image} alt="react logo" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
